4b808c0ff6592412f578eb9503d59459e52c5387
EGAProg <gameswithiy@gmail.com>
2023-03-17 18:57:54 +0300

    Useless commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "header.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    
+    cout << "\nThis program tests the performance of the shit\n";
+    return 0;
+}
